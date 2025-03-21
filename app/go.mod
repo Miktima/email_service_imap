d11e8abdf348a3b75c02c9ced732b6f1e9918d43
Тимофеев Михаил Константинович <m.timofeev@ria.ru>
2025-03-21 17:43:23 +0300

    charset

--- app/go.mod
+++ app/go.mod
@@ -2,9 +2,12 @@ module gotests
 
 go 1.23
 
-require github.com/emersion/go-imap/v2 v2.0.0-beta.5
+require (
+	github.com/emersion/go-imap/v2 v2.0.0-beta.5
+	github.com/emersion/go-message v0.18.1
+)
 
 require (
-	github.com/emersion/go-message v0.18.1 // indirect
 	github.com/emersion/go-sasl v0.0.0-20231106173351-e73c9f7bad43 // indirect
+	golang.org/x/text v0.14.0 // indirect
 )
--- app/main.go
+++ app/main.go
@@ -3,25 +3,34 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 
+	"github.com/emersion/go-imap/v2"
 	"github.com/emersion/go-imap/v2/imapclient"
+	_ "github.com/emersion/go-message/charset"
+	"github.com/emersion/go-message/mail"
 )
+
+type Ctx struct {
+	Client *imapclient.Client
+	Mbox   *imap.SelectData
+}
 
 func main() {
 
-	var client imapclient.Client
+	ctx := new(Ctx)
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/login", LoginHandler(&client))
-	//mux.HandleFunc("/logout", LogoutHandler(&ctx))
-	//mux.HandleFunc("/mail", MailHandler(&ctx))
+	mux.HandleFunc("/login", LoginHandler(ctx))
+	mux.HandleFunc("/logout", LogoutHandler(ctx))
+	mux.HandleFunc("/mail", MailHandler(ctx))
 	http.ListenAndServe(":8080", mux)
 
 }
 
-func LoginHandler(client *imapclient.Client) func(http.ResponseWriter, *http.Request) {
+func LoginHandler(ctx *Ctx) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, req *http.Request) {
 		resp := make(map[string]string)
 		// Проверяем метод запроса - для логина принимаем только POST
@@ -54,11 +63,15 @@ func LoginHandler(client *imapclient.Client) func(http.ResponseWriter, *http.Req
 		if err != nil {
 			log.Fatalf("failed to select INBOX: %v", err)
 		}
+
+		ctx.Client = client
+		ctx.Mbox = selectedMbox
 		log.Printf("INBOX contains %v messages", selectedMbox.NumMessages)
 
 		w.WriteHeader(http.StatusCreated)
 		w.Header().Set("Content-Type", "application/json")
 
+		// Формируем ответ
 		if err == nil {
 			resp["status"] = "Ok"
 			resp["message"] = fmt.Sprintf("INBOX contains %v messages", selectedMbox.NumMessages)
@@ -74,11 +87,11 @@ func LoginHandler(client *imapclient.Client) func(http.ResponseWriter, *http.Req
 	}
 }
 
-func LogoutHandler(client *imapclient.Client) func(http.ResponseWriter, *http.Request) {
+func LogoutHandler(ctx *Ctx) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, req *http.Request) {
 		resp := make(map[string]string)
-		// Проверяем, что контекст определен
-		if client == nil {
+		// Проверяем, что клиент определен
+		if ctx.Client == nil {
 			resp["status"] = "Error"
 			resp["message"] = "Client is down"
 			w.WriteHeader(http.StatusCreated)
@@ -90,9 +103,13 @@ func LogoutHandler(client *imapclient.Client) func(http.ResponseWriter, *http.Re
 			w.Write(jsonResp)
 			return
 		}
-
-		err := client.Logout().Wait()
-
+		// Выходим из аккаунта
+		err := ctx.Client.Logout().Wait()
+		if err != nil {
+			log.Fatalf("Logout error. Err: %s", err)
+		}
+		log.Println("Client logged out :", ctx.Client)
+		// Формируем ответ
 		w.WriteHeader(http.StatusCreated)
 		w.Header().Set("Content-Type", "application/json")
 
@@ -109,5 +126,137 @@ func LogoutHandler(client *imapclient.Client) func(http.ResponseWriter, *http.Re
 			log.Fatalf("Error happened in JSON marshal. Err: %s", err)
 		}
 		w.Write(jsonResp)
+	}
+}
+
+func MailHandler(ctx *Ctx) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, req *http.Request) {
+		resp := make(map[string]string)
+		// Проверяем, что клиент определен
+		if ctx.Client == nil {
+			resp["status"] = "Error"
+			resp["message"] = "Client is down"
+			w.WriteHeader(http.StatusCreated)
+			w.Header().Set("Content-Type", "application/json")
+			jsonResp, err := json.Marshal(resp)
+			if err != nil {
+				log.Fatalf("Error happened in JSON marshal. Err: %s", err)
+			}
+			w.Write(jsonResp)
+			return
+		}
+		log.Println("Mail client:", ctx.Client)
+
+		w.WriteHeader(http.StatusCreated)
+		w.Header().Set("Content-Type", "application/json")
+		// Ошибка, если сообщений в почте нет
+		if ctx.Mbox.NumMessages == 0 {
+			resp["status"] = "Error"
+			resp["message"] = "No messages in the mailbox"
+			log.Fatalf("No messages in the mailbox")
+			jsonResp, err := json.Marshal(resp)
+			if err != nil {
+				log.Fatalf("Error happened in JSON marshal. Err: %s", err)
+			}
+			w.Write(jsonResp)
+		} else {
+			// Send a FETCH command to fetch the message body
+			seqSet := imap.SeqSetNum(1)
+			bodySection := &imap.FetchItemBodySection{}
+			fetchOptions := &imap.FetchOptions{
+				BodySection: []*imap.FetchItemBodySection{bodySection},
+			}
+			fetchCmd := ctx.Client.Fetch(seqSet, fetchOptions)
+			defer fetchCmd.Close()
+
+			msg := fetchCmd.Next()
+			if msg == nil {
+				log.Fatalf("FETCH command did not return any message")
+			}
+
+			// Find the body section in the response
+			var bodySectionData imapclient.FetchItemDataBodySection
+			ok := false
+			for {
+				item := msg.Next()
+				if item == nil {
+					break
+				}
+				bodySectionData, ok = item.(imapclient.FetchItemDataBodySection)
+				if ok {
+					break
+				}
+			}
+			if !ok {
+				log.Fatalf("FETCH command did not return body section")
+			}
+
+			// Read the message via the go-message library
+			mr, err := mail.CreateReader(bodySectionData.Literal)
+			if err != nil {
+				log.Fatalf("failed to create mail reader: %v", err)
+			}
+
+			// Print a few header fields
+			h := mr.Header
+			var subject string
+			if date, err := h.Date(); err != nil {
+				log.Printf("failed to parse Date header field: %v", err)
+			} else {
+				log.Printf("Date: %v", date)
+			}
+			if to, err := h.AddressList("To"); err != nil {
+				log.Printf("failed to parse To header field: %v", err)
+			} else {
+				log.Printf("To: %v", to)
+			}
+			if subject, err := h.Text("Subject"); err != nil {
+				log.Printf("failed to parse Subject header field: %v", err)
+			} else {
+				log.Printf("Subject: %v", subject)
+			}
+
+			// Process the message's parts
+			var body string
+			for {
+				p, err := mr.NextPart()
+				if err == io.EOF {
+					break
+				} else if err != nil {
+					log.Fatalf("failed to read message part: %v", err)
+				}
+
+				switch h := p.Header.(type) {
+				case *mail.InlineHeader:
+					// This is the message's text (can be plain-text or HTML)
+					b, _ := io.ReadAll(p.Body)
+					body = string(b)
+					log.Printf("Inline text: %v", body)
+				case *mail.AttachmentHeader:
+					// This is an attachment
+					filename, _ := h.Filename()
+					log.Printf("Attachment: %v", filename)
+				}
+			}
+
+			if err := fetchCmd.Close(); err != nil {
+				log.Fatalf("FETCH command failed: %v", err)
+			}
+
+			if err == nil {
+				resp["status"] = "Ok"
+				resp["subject"] = subject
+				resp["body"] = body
+			} else {
+				resp["status"] = "Error"
+				resp["message"] = fmt.Sprintf("ERROR: %v", err)
+				log.Fatalf("failed to logout: %v", err)
+			}
+			jsonResp, err := json.Marshal(resp)
+			if err != nil {
+				log.Fatalf("Error happened in JSON marshal. Err: %s", err)
+			}
+			w.Write(jsonResp)
+		}
 	}
 }
